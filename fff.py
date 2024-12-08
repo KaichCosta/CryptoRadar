a1aa8da86db1b9e1b0d9817ebfcfe54bd56eb1f9
Kaích Henrique Costa <kaichhc@gmail.com>
2024-12-08 02:26:25 -0300

    JA TA MUITO FUNCIONAL
    A lógica está pronta, por enquanto só me resta fazer a interface gráfica e mostrar lá os valores e dar escolha de alterar preço de mínimo e máxima das criptos

--- fff.py
+++ fff.py
@@ -4,4 +4,4 @@ time.sleep(3)
 pyautogui.press('enter')
 time.sleep(5)
 
-pyautogui.hotkey('ctrl', 'w')
+pyautogui.hotkey('ctrl, w')
--- index.py
+++ index.py
@@ -45,24 +45,82 @@ while True:
         simbolo = symbols[i].upper()
 
         max = limite_max[i]
-        
+
         min = limite_min[i]
 
     if preco > max:
         print(f'\033[1;31m=== ALERTA DE VENDA===\033[0m')
         print(f'\033[1;31m{simbolo}: Preço atual ({preco}) ultrapassou o limite máximo ({max}).\033[0m')
-    
+        #EU IRIA POR TODA ESSA PARTE DE ANÁLISE DE NOME E TELEFONE FORA DO (IF), MAS ISSO FARIA QUE SE REPETISSE TODA HORA POR CAUSA DO (WHILE) E NÃO SOMENTE NA COMPRA OU VENDA
+        hora = datetime.now()
+
+        webbrowser.open('https://web.whatsapp.com/')
+        time.sleep(10)
+
+        workbook = openpyxl.load_workbook('contatos-notificados.xlsx')
+        pagina_contatos = workbook['Plan1']
+
+        for linha in pagina_contatos.iter_rows(min_row=5):
+            #nome, telefone
+            nome = linha[2].value    
+            telefone = linha[3].value
+
+            #mensagem que será enviada aos contatos
+            msg = f'Olá {nome}, hora de vender {simbolo}, {hora.strftime('%d/%m/%Y, %H:%M')}'
+
+            #link que abrirá o zap
+            link_mensagem_zap = f'https://web.whatsapp.com/send?phone={telefone}&text={quote(msg)}'
+
+            webbrowser.open(link_mensagem_zap)
+
+            time.sleep(15)
+            pyautogui.press('enter')
+            time.sleep(5)
+
+            pyautogui.hotkey('ctrl', 'w')
+            time.sleep(2)
+
+            pyautogui.hotkey('ctrl', 'w')
+            time.sleep(5)
+
+            print('FINALIZADO')
+            break
+    #==============================================
+    #==============================================
     elif preco < min:
         print(f'\033[1;31m=== ALERTA DE COMPRA===\033[0m')
         print(f'\033[1;31m{simbolo}: Preço atual ({preco}) ultrapassou o limite máximo ({min}).\033[0m')
-    
-    else:
-        print(f'\033[1;32m{simbolo} está dentro do limite esperado.\033[0m')
+        hora = datetime.now()
 
-    time.sleep(5)
+        webbrowser.open('https://web.whatsapp.com/')
+        time.sleep(10)
+
+        workbook = openpyxl.load_workbook('contatos-notificados.xlsx')
+        pagina_contatos = workbook['Plan1']
+
+        for linha in pagina_contatos.iter_rows(min_row=5):
+            #nome, telefone
+            nome = linha[2].value    
+            telefone = linha[3].value
+
+            #mensagem que será enviada aos contatos
+            msg = f'Olá {nome}, hora de comprar {simbolo}, {hora.strftime('%d/%m/%Y, %H:%M')}'
+
+            #link que abrirá o zap
+            link_mensagem_zap = f'https://web.whatsapp.com/send?phone={telefone}&text={quote(msg)}'
+
+            webbrowser.open(link_mensagem_zap)
+
+            time.sleep(15)
+            pyautogui.press('enter')
+            time.sleep(5)
+
+            pyautogui.hotkey('ctrl', 'w')
+            time.sleep(2)
+
+            pyautogui.hotkey('ctrl', 'w')
+            time.sleep(5)
+
+            print('FINALIZADO')
+            break
     #E TEM QUE QUANDO MANDAR MENSAGEM APARECER QUAL CRIPTO TEM QUE COMPRAR E VENDER)
-
-
-
-
-
